4ebbf2304fb46baf3760a0dcde84a43075272487
Daniel Xu <dxu@dxuuu.xyz>
2020-08-13 11:14:02 -0700

    Factor out superblock parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
 use std::os::unix::prelude::FileExt;
 use std::path::PathBuf;
 use std::slice;
 
+use anyhow::{bail, Result};
 use structopt::StructOpt;
 
 mod structs;
@@ -20,6 +21,21 @@ struct Opt {
     device: PathBuf,
 }
 
+fn parse_superblock(file: &File) -> Result<BtrfsSuperblock> {
+    let mut superblock: BtrfsSuperblock = unsafe { std::mem::zeroed() };
+    let superblock_size = std::mem::size_of::<BtrfsSuperblock>();
+    let slice;
+    unsafe {
+        slice = slice::from_raw_parts_mut(&mut superblock as *mut _ as *mut u8, superblock_size);
+    }
+    file.read_exact_at(slice, BTRFS_SUPERBLOCK_OFFSET)?;
+    if superblock.magic != BTRFS_SUPERBLOCK_MAGIC {
+        bail!("superblock magic is wrong");
+    }
+
+    Ok(superblock)
+}
+
 fn main() {
     let opt = Opt::from_args();
 
@@ -29,15 +45,5 @@ fn main() {
         .expect("Failed to open path");
 
     // Read superblock
-    let mut superblock: BtrfsSuperblock = unsafe { std::mem::zeroed() };
-    let superblock_size = std::mem::size_of::<BtrfsSuperblock>();
-    let slice;
-    unsafe {
-        slice = slice::from_raw_parts_mut(&mut superblock as *mut _ as *mut u8, superblock_size);
-    }
-    file.read_exact_at(slice, BTRFS_SUPERBLOCK_OFFSET)
-        .expect("failed to read superblock");
-    if superblock.magic != BTRFS_SUPERBLOCK_MAGIC {
-        panic!("superblock magic is wrong");
-    }
+    let superblock = parse_superblock(&file).expect("failed to parse superblock");
 }
